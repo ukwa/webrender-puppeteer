d8170d94b45906fc7cccc779b821e1dcd3666d47
Andrew Jackson <Andrew.Jackson@bl.uk>
2021-06-01 22:58:13 +0100

    Added push of results to WARCPROX WARC records.

--- README.md
+++ README.md
@@ -8,3 +8,33 @@ Note that `npm` can be used to control versioning in `package.json` and tagging.
 Then a `git push origin master --follow-tags`` should build a suitably tagged release.
 
 
+To Do
+=====
+
+ - Check 204 status, throw error if it didn't work
+ - warcprox_prefix parameter and passing that through as an extra header, as per
+   - extra_headers = { "Warcprox-Meta" : json.dumps( { 'warc-prefix' : warc_prefix}) }
+ - har
+ - imagemap
+ - thumbnail?
+ - Tests: simple page, dynamic page, serviceworker, pdf, xml, dead/gone site, unicode URLs
+ - features (ENV or URL?): USER_AGENT_ADDITIONAL, switchDevices, scaleFactor, viewport width/height, memento datetime, 
+
+
+def build_imagemap(page_jpeg, page):
+    html = "<html><head><title>%s [Static version of %s]</title>\n</head>\n<body style=\"margin: 0;\">\n" % (page['title'], page['url'])
+    html = html + '<img src="data:image/jpeg;base64,%s" usemap="#shapes" alt="%s">\n' %( base64.b64encode(page_jpeg).decode('utf-8'), page['title'])
+    html = html + '<map name="shapes">\n'
+    for box in page['map']:
+        if 'href' in box:
+            x1 = box['location']['left']
+            y1 = box['location']['top']
+            x2 = x1 + box['location']['width']
+            y2 = y1 + box['location']['height']
+            html = html + '<area shape=rect coords="%i,%i,%i,%i" href="%s">\n' % (x1,y1,x2,y2,box['href'])
+        else:
+            logger.debug("Skipping box with no 'href': %s" % box)
+    html = html + '</map>\n'
+    html = html + "</body>\n</html>\n"
+    return html
+  
--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const app = require("express")();
 
 const e = require("express");
+const http = require("http");
+const { URL } = require("url");
 const promBundle = require("express-prom-bundle");
 const promClient = require('prom-client');
 const metricsMiddleware = promBundle({includeMethod: true});
@@ -15,10 +17,8 @@ const g3 = new promClient.Gauge({name: 'puppeteer_cluster_queued_count', help: '
 const g4 = new promClient.Gauge({name: 'puppeteer_cluster_error_count', help: 'The total number of error events for this Puppeteer cluster'});
 
 // Get configuration:
-let maxConcurrency = 2;
-if ('PUPPETEER_CLUSTER_SIZE' in process.env) {
-    maxConcurrency = parseInt(process.env.PUPPETEER_CLUSTER_SIZE);
-}
+const maxConcurrency = parseInt(process.env.PUPPETEER_CLUSTER_SIZE || '2', 10);
+const warcproxProxy = process.env.WARCPROX_PROXY || '';
 
   // Set up the browser in the required configuration:
   const browserArgs = {
@@ -38,10 +38,13 @@ if ('PUPPETEER_CLUSTER_SIZE' in process.env) {
   };
   // Add proxy configuration if supplied:
   if (process.env.HTTP_PROXY) {
-    proxy_url = process.env.HTTP_PROXY;
+    var proxy_url = process.env.HTTP_PROXY;
     // Remove any trailing slash:
     proxy_url = proxy_url.replace(/\/$/,'')
     browserArgs.args.push(`--proxy-server=${proxy_url}`);
+    // Record parts:
+    var proxy_host = new URL(proxy_url).hostname;
+    var proxy_port = new URL(proxy_url).port;
   }
   console.log('Browser arguments: ', browserArgs);
 
@@ -56,10 +59,85 @@ if ('PUPPETEER_CLUSTER_SIZE' in process.env) {
         timeout: 5*60*1000, // Large 5min timeout by default
     });
 
+    function post_to_warcprox(uri, data, contentType, warcType='resource', location=null, extraHeaders=[]) {
+      console.log(`Attempting to POST data for ${uri}`);
+        const options = {
+            host: proxy_host,
+            port: proxy_port,
+            path: uri,
+            method: 'WARCPROX_WRITE_RECORD',
+            headers: {
+              'Content-Type': contentType,
+              'WARC-Type': warcType,
+              'Host': 'ignored.com',
+              'Content-Length': Buffer.byteLength(data)
+            }
+        };
+
+        if( location ) {
+          options.headers['Location'] = location;
+        }
+          
+          const req = http.request(options, (res) => {
+            console.log(`STATUS: ${res.statusCode}`);
+            console.log(`HEADERS: ${JSON.stringify(res.headers)}`);
+            res.setEncoding('utf8');
+            res.on('data', (chunk) => {
+              console.log(`BODY: ${chunk}`);
+            });
+            res.on('end', () => {
+              console.log('No more data in response.');
+            });
+          });
+          
+          req.on('error', (e) => {
+            console.error(`problem with request: ${e.message}`);
+          });
+          
+          // Write data to request body
+          req.write(data);
+          req.end();
+    }
+
     await cluster.task(async ({ page, data: url }) => {
         update_metrics()
-        // make a screenshot
+        // Render the page
         har = await render_page(page, url);
+        // POST records to warcprox if available:
+        if ( warcproxProxy ) {
+          if ( 'url' in har.urls ) {
+            console.log("Render appears to have worked. POSTing results to warcprox..."); 
+            const finalUrl = har.urls.url;
+            // DOM
+            post_to_warcprox(
+              `onreadydom:${url}`,
+              new Buffer.from( har.finalPage.content, 'base64' ),
+              har.finalPage.contentType,
+              location=finalUrl,
+            );
+            // HAR
+            post_to_warcprox(
+              `har:${url}`,
+              JSON.stringify(har.har),
+              'application/json',
+              location=finalUrl,
+            );
+            // Rendered Elements:
+            har.renderedElements.forEach(function(relem) {
+              console.log(` - ${relem.selector} - ${relem.contentType}`);
+              var uriPrefix = 'screenshot';
+              if( relem.contentType == 'application/pdf') {
+                uriPrefix = 'pdf';
+              }
+              post_to_warcprox(
+                `${uriPrefix}:${url}#xpointer(${relem.selector})`,
+                new Buffer.from( relem.content, 'base64' ),
+                relem.contentType,
+                location=finalUrl,
+              );
+            });
+          }
+        }
         return har;
     });
 
@@ -71,10 +149,10 @@ if ('PUPPETEER_CLUSTER_SIZE' in process.env) {
         g4.set(cluster.errorCount);
     }
 
-    // setup server
+    // setup rendering endpoint:
     app.get('/render', async function (req, res) {
         if (!req.query.url) {
-            return res.end('Please specify url like this: ?url=https://example.com');
+            return res.status(400).end('Please specify url like this: ?url=https://example.com\n');
         }
         try {
             const har = await cluster.execute(req.query.url);
@@ -102,6 +180,11 @@ if ('PUPPETEER_CLUSTER_SIZE' in process.env) {
             // catch error
             res.end('Error: ' + err.message);
         }
+    });
+
+    // setup service-is-available endpoint:
+    app.get('/', async function (req, res) {
+      res.end('OK');
     });
 
     const port = process.env.PORT || 3000;
